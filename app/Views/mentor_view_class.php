90b63876d6c41aa2c5b2caf4c78175cef7d88036
Salsabila Fatma Aripa <114417418+bilaaripa@users.noreply.github.com>
2024-06-18 21:10:35 +0700

    update mentor_view_class

--- app/Views/mentor_view_class.php
+++ app/Views/mentor_view_class.php
@@ -6,24 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-Learning App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- Link to Inter font from Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
+
     <style>
         body {
             margin: 0;
             padding-top: 20px;
             font-family: Inter, sans-serif;
-            overflow: hidden;
         }
 
+        /* Sidebar styling */
         .sidebar {
             height: 100vh;
             width: 250px;
             position: fixed;
             top: 0;
             left: 0;
-            background-color: #4829B2;
+            background-color: #002979;
             color: #fff;
-            padding: 20px;
+            padding-top: 40px;
+            padding-right: 20px;
+            padding-bottom: 20px;
+            padding-left: 20px;
         }
 
         .sidebar-brand {
@@ -34,7 +40,7 @@
 
         .sidebar-nav {
             list-style: none;
-            padding-left: 0;
+            padding-left: 20px;
         }
 
         .sidebar-nav .nav-item {
@@ -42,11 +48,11 @@
         }
 
         .sidebar-nav .nav-item a {
-            color: #fff;
-            text-decoration: none;
+            font-family: 'Inter', sans-serif;
         }
 
         .nav-item1 {
+            list-style: none;
             padding-top: 360px;
         }
 
@@ -55,7 +61,7 @@
             padding: 0.5rem 1rem;
             text-decoration: none;
             color: red;
-            background-color: #F1E8F6;
+            background-color: white;
             border-radius: 10px;
         }
 
@@ -68,7 +74,9 @@
             display: flex;
             align-items: center;
             justify-content: flex-end;
-            padding: 30px 30px 0 0;
+            padding: 30px;
+            padding-bottom: 0px;
+
         }
 
         .user-info img {
@@ -83,197 +91,233 @@
             color: black;
         }
 
-        .assignment-card {
+        .container img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+            /* Center the image */
+        }
+
+        .desktop-1-Mss {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+
+        .auto-group-39fb-PpZ {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .vector-zxm {
+            width: 50px;
+            height: auto;
+        }
+
+        .navbar-43-WRK {
+            display: flex;
+            align-items: center;
+        }
+
+        .auto-group-zq2d-y41 {
+            display: flex;
+            align-items: center;
+        }
+
+        .frame-63-hVo {
+            display: flex;
+            align-items: center;
+        }
+
+        .frame-64-3Zf {
+            display: flex;
+            align-items: center;
+            margin-right: 20px;
+        }
+
+        .raphael-books-XNM {
+            width: 30px;
+            height: auto;
+            margin-right: 10px;
+        }
+
+        .media-ilmu-pcM,
+        .catalog-JGd,
+        .delivery-R6M,
+        .delivery-LUD {
+            margin: 0;
+            padding: 0;
+            margin-right: 20px;
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .frame-65-5Au {
+            display: flex;
+            align-items: center;
+        }
+
+        .ellipse-1-Nvh {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            background-color: #fff;
+            margin-right: 10px;
+        }
+
+        .delivery-1Ts {
+            margin: 0;
+            padding: 0;
+            margin-right: 10px;
+            color: #fff;
+        }
+
+        .iconamoon-arrow-up-2-light-jem {
+            width: 20px;
+            height: auto;
+        }
+
+
+        .bg-success {
+            background-color: #DDFEDF !important;
+            /* Hijau */
+        }
+
+        .bg-primary {
+            background-color: #DDEEFE !important;
+            /* Biru */
+        }
+
+        .bg-purple {
+            background-color: #E8DDFE !important;
+            /* Ungu */
+        }
+
+        .bg-pink {
+            background-color: #FEDDDD !important;
+            /* Pink */
+        }
+
+        .bg-orange {
+            background-color: #FFF0BB !important;
+            /* Kuning */
+        }
+
+        .navbar-nav .nav-link {
+            outline: none;
+        }
+
+        .navbar-nav .nav-item.active a {
+            color: blue;
+            font-weight: bold;
+        }
+
+        .card {
             height: 100%;
-            width: 90%;
             display: flex;
             flex-direction: column;
-            padding: 20px;
-            margin: 20px;
-            border-radius: 7px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
             background-color: #fff;
         }
 
-        h1 {
-            font-size: 26px;
-            font-weight: 700;
-            color: #333;
-        }
-
-        h2 {
-            margin: 20px 0;
-            font-size: 18px;
-            font-weight: 600;
-            color: #333;
-        }
-
-        textarea.form-control {
+        .card img {
+            flex-grow: 1;
+            object-fit: cover;
+            height: 100%;
             width: 100%;
-            height: 150px;
-            resize: both;
         }
 
-        .custom-button {
+        .card-body p.card-text {
             display: inline-block;
-            padding: 10px 20px;
-            color: white;
-            background-color: #28a745;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            text-align: center;
-            text-decoration: none;
-            font-size: 16px;
-            width: 100%;
-            margin: 10px 0;
-        }
-
-        .custom-button:hover {
-            background-color: #218838;
-        }
-
-        /* Custom table styling */
-        .assignment table {
-            width: 80%;
-            border-collapse: collapse;
-        }
-
-        .assignment th,
-        .assignment td {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
-
-        .assignment th {
-            background-color: #4829B2;
-            color: #fff;
-            text-align: left;
-        }
-
-        .assignment tr:nth-child(even) {
-            background-color: #f9f9f9;
-        }
-
-        .assignment tr:hover {
-            background-color: #ddd;
-        }
-
-        .assignment td a.btn {
-            padding: 5px 10px;
-            font-size: 14px;
+            padding: 0.5rem 1rem;
+            border-radius: 0.25rem;
+            color: #000;
+            background-color: #FFF0BB;
+            /* Set text color to white */
         }
     </style>
 </head>
 
 <body>
-    <nav class="sidebar">
-        <div style="padding-bottom: 40px; text-align: center; color: white;">
-            <a class="sidebar-brand" href="#">
-                <img src="./assets/book.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                E-learningApp
-            </a>
+    <header>
+        <div class="sidebar">
+            <div style="padding-bottom: 40px; text-align: center; color: white;">
+                <a class="sidebar-brand" href="#">
+                    <img src="./assets/book.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    E-learningApp
+                </a>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="nav-item">
+                    <span style="display: flex; align-items: center; padding-left: 20px;">
+                        <img src="./assets/beranda.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
+                        <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('./') ?>">Beranda</a>
+                    </span>
+                </li>
+                <li class="nav-item">
+                    <span style="display: flex; align-items: center; padding-left: 20px;">
+                        <img src="./assets/kelas.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
+                        <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('#') ?>">Kelas</a>
+                    </span>
+                </li>
+                <li class="nav-item">
+                    <span style="display: flex; align-items: center; padding-left: 20px;">
+                        <img src="./assets/jadwal.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
+                        <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('./jadwal') ?>">Jadwal</a>
+                    </span>
+                </li>
+                <li class="nav-item1 text-center">
+                    <a class="nav-link active" aria-current="page" href="<?= base_url('./login') ?>">Keluar</a>
+                </li>
+            </ul>
         </div>
-        <ul class="sidebar-nav">
-            <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/beranda.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('#') ?>">Beranda</a>
-                </span>
-            </li>
-            <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/kelas.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('class') ?>">Kelas</a>
-                </span>
-            </li>
-            <!-- <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/jadwal.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('./jadwal') ?>">Jadwal</a>
-                </span>
-            </li>
-            <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/nilai.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('./nilai') ?>">Nilai</a>
-                </span>
-            </li> -->
-            <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/jadwal.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('assignments/create') ?>">Buat Tugas</a>
-                </span>
-            </li>
-            <li class="nav-item">
-                <span style="display: flex; align-items: center; padding-left: 20px;">
-                    <img src="./assets/materi.png" alt="Logo" width="20" height="20" class="d-inline-block align-text-top">
-                    <a class="nav-link active" style="padding-left: 5px;" aria-current="page" href="<?= base_url('materials/create') ?>">Materi</a>
-                </span>
-            </li>
-        </ul>
-        <ul class="nav-item1">
-            <a href="<?= base_url('logout') ?>">
-                Keluar
-            </a>
-        </ul>
-    </nav>
+    </header>
 
-    <div class="container" style="margin-left: 270px;">
-        <header class="d-flex justify-content-between align-items-center">
+    <div style="margin-left: 270px;">
+        <div class="d-flex justify-content-between align-items-center" style=" padding-bottom:10px;">
             <div class="container mt-4" style="width: 81%;">
                 <form class="form-inline">
                     <div class="d-flex justify-content-between align-items-center">
                         <input class="form-control" type="search" placeholder="Cari kelas sekarang..." id="searchInput" aria-label="Search">
-                        <button class="btn btn-primary" type="button" onclick="searchPelajaran()">Cari</button>
+                        <button class="btn btn-primary" type="button" onclick="searchKelas()">Cari</button>
                     </div>
                 </form>
-            </div>
-            <div class="container mt-4" style="width: 20%">
-                <a href="<?php echo site_url('class/create') ?>" class="btn btn-primary" style="background-color: #4829B2; color: #ffffff;">+ Add Class</a>
             </div>
 
             <div class="user-info">
                 <img src="./assets/ellipse-1-bg-eyb.png" alt="Logo" width="48" height="48" class="d-inline-block align-text-top">
                 <span>
-                    <div class="user-name"><?php echo session()->get('username'); ?></div>
+                    <div class="user-name">Melanie Refman</div>
                     <div class="user-name1" style="font-size: 13px;">Mentor</div>
                 </span>
             </div>
-        </header>
+        </div>
 
-        <!-- table -->
-        <div class="container mt-4">
-            <div class="assignment" style="margin-top: 20px;">
-                <h1>Class Room</h1>
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Class Name</th>
-                            <th>Description</th>
-                            <th>Quota</th>
-                            <th>Detail</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($class as $classes) : ?>
-                        <tr>
-                            <td><?= $classes['id'] ?></td>
-                            <td><?= $classes['class_name'] ?></td>
-                            <td><?= $classes['class_description'] ?></td>
-                            <td><?= $classes['quota'] ?></td>
-                            <td><a href="<?php echo site_url('class/detail/'. $classes['id']) ?>" class="btn btn-warning btn-sm">Detail</a></td>
-                            <td><a href="<?php echo site_url('class/update/' . $classes['id']) ?>" class="btn btn-success btn-sm">Update</a></td>
-                            <td><a href="<?php echo base_url('class/delete/'. $classes['id']) ?>" class="btn btn-danger btn-sm">Delete</a></td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
+        <!-- Add a container for kelas -->
+       <div style="margin: 20px;">
+        <div class="row" id="resultKelas">
+            <h3>List of Class</h3>
+            <?php foreach ($class as $classes) : ?>
+                <div class="col-md-3 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="<?= base_url('assets/blue.jpg'); ?>" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= esc($classes['class_name']); ?></h5>
+                            <p class="card-text"><?= esc($classes['class_description']); ?></p>
+                            <br>
+                            <a href="<?php echo site_url('class/detail/'. $classes['id']) ?>" class="btn btn-primary">Detail</a>
+                            <a href="<?php echo site_url('class/update/'. $classes['id']) ?>" class="btn btn-success">Update</a>
+                            <a href="<?php echo site_url('class/delete/'. $classes['id']) ?>" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
+
 </html>
